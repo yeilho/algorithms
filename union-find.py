3b22bc8ec0d4786fea00dbec1d548279bc16b6a9
Ilho <ilhoye@yahoo.com>
2017-04-08 21:38:22 -0700

    Create union-find.py

---
+++ union-find.py
@@ -0,0 +1,32 @@
+#
+# simple example of union-find
+#
+
+def make_set(n):
+  arr = list(xrange(0,n))
+  return arr
+
+def union(arr, i, j):
+  arr[j] = i
+
+def find(arr, i):
+  if arr[i] == i:
+    return i
+  # path compression
+  arr[i] = find(arr, arr[i])
+  return arr[i]
+
+def number_of_connected(n, edges):
+  arr = make_set(n)
+  for x,y in edges:
+    union(arr, x, y)
+  ret = set()
+  for i in arr:
+    ret.add(find(arr, i))
+  return len(ret)
+
+input = [[0, 1], [1, 2], [3, 4]]
+#input = [[0, 1], [1, 2], [2, 3], [3, 4]]
+num = number_of_connected(5, input)
+
+print "num: %d" % num
